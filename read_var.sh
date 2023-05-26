ca7a3ad9e94594ca29f5e14894bdb822cb767eeb
hongzheduan <hongzheduan@gmail.com>
2023-05-26 14:00:18 -0400

    Update read_var.sh

--- read_var.sh
+++ read_var.sh
@@ -7,9 +7,9 @@ cat id.csv | sed 's/,$/,NA/g' > ${DS_NAME}.csv
 for TARGET in ${TARGET_FIELDS};do
   N_TARGET=$(awk -v target="$TARGET" 'BEGIN{FS=OFS="\t"}$1==target{printf("%s_%s\n", $5,$4)}' dictionary_final.tsv | wc -l)
   for EACH_TARGET in $(eval echo "{1..$N_TARGET}");do
-      COHORT_N=$(awk -v target="$TARGET" 'BEGIN{FS=OFS="\t"}$1==target{printf("%s_%s\n", $5,$4)}' dictionary_final.tsv | head -${EACH_TARGET} | tail -1 | awk '{split($1,a,"_"); print a[1]}')
-      COLUMN_N=$(awk -v target="$TARGET" 'BEGIN{FS=OFS="\t"}$1==target{printf("%s_%s\n", $5,$4)}' dictionary_final.tsv | head -${EACH_TARGET} | tail -1 | awk '{split($1,a,"_"); print a[2]}')
-      echo field $TARGET has $N_TARGET records, working on cohort number ${COHORT_N} and column number ${COLUMN_N}
+      COHORT_N=$(awk -v target="$TARGET" 'BEGIN{FS=OFS="\t"}$1==target{printf("%s_%s\n", $5,$4)}' dictionary_final.tsv | head -$EACH_TARGET | tail -1 | awk '{split($1,a,"_"); print a[1]}')
+      COLUMN_N=$(awk -v target="$TARGET" 'BEGIN{FS=OFS="\t"}$1==target{printf("%s_%s\n", $5,$4)}' dictionary_final.tsv | head -$EACH_TARGET | tail -1 | awk '{split($1,a,"_"); print a[2]}')
+      echo field $TARGET has $N_TARGET records, working on cohort number $COHORT_N and column number $COLUMN_N
       if [ "$COHORT_N" -le 9 ]; then
           ANALYRIC_FILE_NAME="pheno_AD-Infect_all_00${COHORT_N}_v20230419_FN_raw_participant"
       else
